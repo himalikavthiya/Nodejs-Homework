3305115957d05577ec7ac347e3ea87e9e36d8659
https://github.com/himalikavthiya <himalikavthiya3@gmail.com>
2023-09-11 18:03:57 +0530

    e-comm api

--- New-EcommApp/API Hit/index.js
+++ New-EcommApp/API Hit/index.js
@@ -18,19 +18,33 @@ function renderTable() {
    ${data
      .map(
        (item) => `
-      <div class="card">
-      <img src="${item.product_img}" class="card-image" alt="${item.product_name}">
-      <div class="card-body">
-        <h2 class="card-title">${item.product_name}</h2>
-        <p class="card-description">${item.description}</p>
-        <p class="card-price">$${item.price}</p>
-        <p class="card-category">${item.category.category_name}</p>
-        <p class="card-subcategory">${item.subcategory.subcategory_name}</p>
-        <p class="card-subchildcat">${item.subchildcate.subchildcat_name}</p>
-      </div>
-    </div>
+       <div class="products" id="iphone-x">
+       <img class="product-image" src="${item.product_img}"/>
+       <p class="product-name">${item.product_name}</p>
+       <p class="product-description">${item.description}</p>
+       <p class="product-price">$${item.price}</p>
+       <p class="product-category">${item.category.category_name}</p>
+       <p class="product-subcategory">${item.subcategory.subcategory_name}</p>
+       <p class="product-subchildcat">${item.subchildcate.subchildcat_name}</p>
+       <button class="add-to-cart" id='test'>ADD TO CART</button>
+   </div>
   `
      )
      .join("")}
     `;
 }
+
+
+
+{/* <div class="card">
+<img src="${item.product_img}" class="card-image" alt="${item.product_name}">
+<div class="card-body">
+  <h2 class="card-title">${item.product_name}</h2>
+  <p class="card-description">${item.description}</p>
+  <p class="card-price">$${item.price}</p>
+  <p class="card-category">${item.category.category_name}</p>
+  <p class="card-subcategory">${item.subcategory.subcategory_name}</p>
+  <p class="card-subchildcat">${item.subchildcate.subchildcat_name}</p>
+  <button class="add-to-cart" id='test'>ADD TO CART</button>
+</div>
+</div> */}

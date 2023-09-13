8b4f853617f0b1ff1b97aeedcc51de563b5cf161
https://github.com/himalikavthiya <himalikavthiya3@gmail.com>
2023-09-13 08:40:00 +0530

    e-comm app

--- New-EcommApp/API Hit/index.js
+++ New-EcommApp/API Hit/index.js
@@ -36,15 +36,3 @@ function renderTable() {
 
 
 
-{/* <div class="card">
-<img src="${item.product_img}" class="card-image" alt="${item.product_name}">
-<div class="card-body">
-  <h2 class="card-title">${item.product_name}</h2>
-  <p class="card-description">${item.description}</p>
-  <p class="card-price">$${item.price}</p>
-  <p class="card-category">${item.category.category_name}</p>
-  <p class="card-subcategory">${item.subcategory.subcategory_name}</p>
-  <p class="card-subchildcat">${item.subchildcate.subchildcat_name}</p>
-  <button class="add-to-cart" id='test'>ADD TO CART</button>
-</div>
-</div> */}
